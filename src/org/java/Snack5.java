ec407c0f8d271ef0f14bf8bc9fb211d359c0f8cd
MarcoMazzilli <mazzillimarco97@gmail.com>
2023-09-14 16:55:46 +0200

    [Refactoring] Snack5

--- src/org/java/Snack5.java
+++ src/org/java/Snack5.java
@@ -6,46 +6,57 @@ public class Snack5 {
 	
 	public static void main(String[] args) {
 		
-		//Input
+		String userInput = null;
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("Inserisci una stringa : ");
-		String input = sc.nextLine();
 		
-		System.out.println("input: " + input);
+		do {
+			
+			//Input
+			
+			
+			userInput = sc.nextLine();
+			System.out.println("userInput: " + userInput);
+			
+			// /Input
+			
+			// Data
+			char[] arrayInput = userInput.toCharArray();
+			int numbers = 0;
+			int letters = 0;
+			int specialChar = 0;
+			// /Data
+			
+			//Logic
+				
+				for (int i = 0; i < userInput.length(); i++) {
+					
+					if(Character.isDigit(arrayInput[i])){
+						
+						numbers++;
+						
+					}else if(Character.isAlphabetic(arrayInput[i])){
+						
+						letters++;
+						
+					}else {
+						specialChar++;
+					}
+				}
+					
+			// /Logic
+			
+			//Output
+			System.out.println("Numeri : " + numbers);
+			System.out.println("Lettrere : " + letters);
+			System.out.println("Caratteri speciali : " + specialChar);
+			// /Output
+			
+			
+		} while (!userInput.equals("0"));
 		
+		System.out.println("Ho finito : " + userInput.equals("0"));
 		sc.close();
-		// /Input
-		
-		// Data
-		char[] arrayInput = input.toCharArray();
-		int numbers = 0;
-		int letters = 0;
-		int specialChar = 0;
-		// /Data
-		
-		//Logic
-		for (int i = 0; i < input.length(); i++) {
-			
-			if(Character.isDigit(arrayInput[i])){
-				
-				numbers++;
-				
-			}else if(Character.isAlphabetic(arrayInput[i])){
-				
-				letters++;
-				
-			}else {
-				specialChar++;
-			}
-			
-		}
-		// /Logic
-		
-		//Output
-		System.out.println("Numeri : " + numbers);
-		System.out.println("Lettrere : " + letters);
-		System.out.println("Caratteri speciali : " + specialChar);
-		// /Output
+	 }
 	}
-}
